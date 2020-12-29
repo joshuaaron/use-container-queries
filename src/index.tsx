090ec24a5de42f9d79fa1d3c00ebad0b48c6b156
Josh Reynolds <josh@askottentertainment.com>
2020-12-29 00:22:47 -0800

    feat: added core hook functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,121 @@
-import * as React from 'react';
+import { RefCallback, useCallback, useRef, useState } from 'react';
+import { useIsomorphicLayoutEffect } from './utils';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+export type QueryBreakpoints = {
+    [key: string]: [number, number?];
 };
+
+export type ContainerQueryProps = {
+    breakpoints: QueryBreakpoints;
+    ignoreDimensions?: boolean;
+};
+
+type ContainerQueryState = {
+    active: string;
+    width: number;
+};
+
+export type ContainerQueryResult<T> = {
+    assignRef: RefCallback<T>;
+    width: number;
+    active: string;
+};
+
+export function useContainerQueries<T extends HTMLElement>({
+    breakpoints,
+    ignoreDimensions = false,
+}: ContainerQueryProps): ContainerQueryResult<T> {
+    const initialBreakpoint = Object.keys(breakpoints)[0];
+    const [state, setState] = useState<ContainerQueryState>({
+        active: initialBreakpoint,
+        width: 0,
+    });
+
+    const observerRef = useRef<ResizeObserver | null>(null);
+    const elementRef = useRef<T | null>(null);
+
+    const matchBreakpoint = useCallback(
+        (prevActive, width) => {
+            let currentActive;
+            for (const [key, [min, max]] of Object.entries(breakpoints)) {
+                if (width >= min) {
+                    if (max === undefined) {
+                        currentActive = key;
+                        break;
+                    } else if (width <= max) {
+                        currentActive = key;
+                        break;
+                    }
+                }
+            }
+
+            return {
+                active: currentActive || prevActive,
+                width,
+            };
+        },
+        [breakpoints]
+    );
+
+    const handleResize = useCallback(
+        (width: number) => {
+            if (ignoreDimensions) {
+                const { active } = matchBreakpoint(state.active, width);
+
+                if (active !== state.active) {
+                    setState((prev) => ({ ...prev, active }));
+                }
+            } else {
+                setState(matchBreakpoint(state.active, width));
+            }
+        },
+        [state.active, ignoreDimensions, matchBreakpoint]
+    );
+
+    useIsomorphicLayoutEffect(() => {
+        if (!observerRef.current) {
+            observerRef.current = new ResizeObserver(([entry]) => {
+                let width;
+                if (entry.borderBoxSize) {
+                    width = Math.round(
+                        Array.isArray(entry.borderBoxSize)
+                            ? entry.borderBoxSize[0].inlineSize
+                            : entry.borderBoxSize.inlineSize
+                    );
+                } else {
+                    width = Math.round(entry.contentRect.width);
+                }
+
+                handleResize(width);
+            });
+        }
+
+        if (elementRef.current) {
+            observerRef.current.observe(elementRef.current, {
+                box: 'border-box',
+            });
+        }
+
+        return () => {
+            observerRef.current?.disconnect();
+            observerRef.current = null;
+        };
+    }, []);
+
+    const assignRef = useCallback((node: T | null) => {
+        if (elementRef.current) {
+            observerRef.current?.unobserve(elementRef.current);
+        }
+
+        elementRef.current = node;
+        if (node) {
+            observerRef.current?.observe(node);
+        }
+    }, []);
+
+    return {
+        assignRef,
+        width: state.width,
+        active: state.active,
+    } as const;
+}
