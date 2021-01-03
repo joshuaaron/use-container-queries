d696a7304463e86cd1464e3e80e5fe09bb4e3f58
Josh Reynolds <josh@askottentertainment.com>
2021-01-02 23:44:20 -0800

    refactor: change naming of props, added comments to types

--- src/index.tsx
+++ src/index.tsx
@@ -2,32 +2,50 @@ import { RefCallback, useCallback, useRef, useState } from 'react';
 import { useIsomorphicLayoutEffect } from './utils';
 
 export type QueryBreakpoints = {
+    /** the values should be consisted of a min and max number, except for your largest breakpoint where it would be the min value and anything above */
     [key: string]: [number, number?];
 };
 
 export type ContainerQueryProps = {
+    /** Explicit key/value map of the predefined breakpoints you wish to use. Each value should have a min and max range (Eg { small: [0, 300], med: [301, 600]} ) */
     breakpoints: QueryBreakpoints;
+    /** Flag to ignore width updates if you are only interested in when a breakpoint changes. App will forego updating state for the width */
     ignoreDimensions?: boolean;
 };
 
 type ContainerQueryState = {
-    active: string;
+    current: string;
     width: number;
 };
 
 export type ContainerQueryResult<T> = {
+    /** Callback ref to be assigned to the containing DOM node the user wishes to observe for changes. */
     assignRef: RefCallback<T>;
+    /** The current 'active' breakpoint. This key will match from one of the key/value pairs from the breakpoints supplied to the hook */
+    current: string;
+    /** Current width of the observed element */
     width: number;
-    active: string;
 };
 
+/**
+ * useContainerQueries.
+ *
+ * A react hook utilizing the Resize Observer API that allows a user to track an elements changing width
+ * with predefined breakpoint ranges, in order to react to the changes, such as responsive styling, where
+ * the user can style DOM elements based on their container, rather than the browser viewport.
+ *
+ * @param {QueryBreakpoints} breakpoints A key/value mapping of explicit breakpoint ranges to be
+ * tested against the current observed elements width to find a match.
+ * @param {boolean} ignoreDimensions Flag stating the user doesn't care about the changing container width,
+ * and only the breakpoint changes. The app will only update state on these changes.
+ */
 export function useContainerQueries<T extends HTMLElement>({
     breakpoints,
     ignoreDimensions = false,
 }: ContainerQueryProps): ContainerQueryResult<T> {
     const initialBreakpoint = Object.keys(breakpoints)[0];
     const [state, setState] = useState<ContainerQueryState>({
-        active: initialBreakpoint,
+        current: initialBreakpoint,
         width: 0,
     });
 
@@ -50,44 +68,46 @@ export function useContainerQueries<T extends HTMLElement>({
             }
 
             return {
-                active: currentActive || prevActive,
+                current: currentActive || prevActive,
                 width,
             };
         },
         [breakpoints]
     );
 
+    /**
+     * Callback triggered on from the resize observer on change.
+     */
     const handleResize = useCallback(
-        (width: number) => {
-            if (ignoreDimensions) {
-                const { active } = matchBreakpoint(state.active, width);
+        ([entry]: readonly ResizeObserverEntry[]) => {
+            let width;
+            if (entry.borderBoxSize) {
+                width = Math.round(
+                    Array.isArray(entry.borderBoxSize)
+                        ? entry.borderBoxSize[0].inlineSize
+                        : entry.borderBoxSize.inlineSize
+                );
+            } else {
+                width = Math.round(entry.contentRect.width);
+            }
 
-                if (active !== state.active) {
-                    setState((prev) => ({ ...prev, active }));
+            // Allow user to opt of updating the state on width changes, and only for breakpoint changes
+            if (ignoreDimensions) {
+                const { current } = matchBreakpoint(state.current, width);
+
+                if (current !== state.current) {
+                    setState((prev) => ({ ...prev, current }));
                 }
             } else {
-                setState(matchBreakpoint(state.active, width));
+                setState(matchBreakpoint(state.current, width));
             }
         },
-        [state.active, ignoreDimensions, matchBreakpoint]
+        [state.current, ignoreDimensions, matchBreakpoint]
     );
 
     useIsomorphicLayoutEffect(() => {
         if (!observerRef.current) {
-            observerRef.current = new ResizeObserver(([entry]) => {
-                let width;
-                if (entry.borderBoxSize) {
-                    width = Math.round(
-                        Array.isArray(entry.borderBoxSize)
-                            ? entry.borderBoxSize[0].inlineSize
-                            : entry.borderBoxSize.inlineSize
-                    );
-                } else {
-                    width = Math.round(entry.contentRect.width);
-                }
-
-                handleResize(width);
-            });
+            observerRef.current = new ResizeObserver(handleResize);
         }
 
         if (elementRef.current) {
@@ -100,8 +120,10 @@ export function useContainerQueries<T extends HTMLElement>({
             observerRef.current?.disconnect();
             observerRef.current = null;
         };
-    }, []);
+    }, [handleResize]);
 
+    // Ref callback passed to user
+    // todo: allow user to provide their own ref.
     const assignRef = useCallback((node: T | null) => {
         if (elementRef.current) {
             observerRef.current?.unobserve(elementRef.current);
@@ -116,6 +138,6 @@ export function useContainerQueries<T extends HTMLElement>({
     return {
         assignRef,
         width: state.width,
-        active: state.active,
+        current: state.current,
     } as const;
 }
